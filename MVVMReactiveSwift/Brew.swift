921af96233d1e62598d9fb55de91ddf1e1b7a5c2
Jakk Sut <jakks@Jakks-iMac.local>
2017-09-05 00:50:26 +0700

    ‘init’

---
+++ MVVMReactiveSwift/Brew.swift
@@ -0,0 +1,11 @@
+//
+//  Brew.swift
+//  MVVMReactiveSwift
+//
+//  Created by Jakkrits on 2/21/2559 BE.
+//  Copyright © 2559 App Illustrator. All rights reserved.
+//
+
+struct Brew {
+    var temp = 0.0
+}
---
+++ MVVMReactiveSwift/BrewViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MVVMReactiveSwift
+//
+//  Created by Jakkrits on 2/21/2559 BE.
+//  Copyright © 2559 App Illustrator. All rights reserved.
+//
+
+import UIKit
+
+class BrewViewController: UIViewController {
+
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
